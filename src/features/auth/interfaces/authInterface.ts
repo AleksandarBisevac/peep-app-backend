a8bcc29de58d754666fc49f8cafbbf948140fba0
AleksandarBisevac <alek.bisevac@gmail.com>
2023-02-16 22:17:32 +0100

    chore: auth schema model added

---
+++ src/features/auth/interfaces/authInterface.ts
@@ -0,0 +1,47 @@
+import { Document } from 'mongoose';
+import { ObjectId } from 'mongodb';
+
+// we define global interface for express request object to add currentUser property
+// this is done to avoid adding currentUser property to every request object
+// we can access currentUser property in any route handler by using req.currentUser
+// currentUser property is optional because it is not available in all routes like signup and signin
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: AuthPayload;
+    }
+  }
+}
+
+export interface AuthPayload {
+  userId: string;
+  uId: string;
+  email: string;
+  username: string;
+  avatarColor: string;
+  iat?: number;
+}
+
+export interface IAuthDocument extends Document {
+  _id: string | ObjectId;
+  uId: string;
+  username: string;
+  email: string;
+  password?: string;
+  avatarColor: Date;
+  comparePassword: (password: string) => Promise<boolean>;
+  hashPassword: (password: string) => Promise<string>;
+}
+
+export interface ISignUpData {
+  _id: ObjectId;
+  uId: string;
+  email: string;
+  username: string;
+  password: string;
+  avatarColor: string;
+}
+
+export interface IAuthJob {
+  value?: string | IAuthDocument;
+}
